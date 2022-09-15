90c942b822c96c61dd0ce38c551c810883dd40f8
Miguel Guthridge <hdsq@outlook.com.au>
2022-09-16 01:36:19 +1000

    Add some UI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bmp::{self, Image, Pixel, open};
-use std::io::{stdout, Write};
+use std::{io::{stdout, Write, self}, process::exit};
 
 type Coordinate = (u32, u32);
 
@@ -42,7 +42,7 @@ fn load(path: &str) -> Image {
  */
 fn query_user(questions: Vec<&String>) -> Vec<String> {
 
-    let responses = Vec::new();
+    let mut responses = Vec::new();
 
     for question in questions.iter() {
         print!("{}", &question[..]);
@@ -91,14 +91,17 @@ fn draw_pixel(mut img: Image, coord: Coordinate, color: Pixel) -> Image {
 /**
  * Draw line (vertical, horizontal, diagonal)
  */
-fn draw_line(mut img: Image, start: Coordinate, end: Coordinate, color: Pixel) -> Image {
+fn draw_line(mut img: Image, mut start: Coordinate, mut end: Coordinate, color: Pixel) -> Image {
     fn get_y(start: Coordinate, end: Coordinate, x: u32) -> u32 {
         f64::round(
-            (end.1 - start.1) as f64
+            (end.1 as i32 - start.1 as i32) as f64
             / (end.0 - start.0) as f64
             * (x - start.0) as f64
             + start.1 as f64
         ) as u32
+    }
+    if start.0 < end.0 {
+        (end, start) = (start, end);
     }
     for x in start.0..=end.0 {
         img = draw_pixel(img, (x, get_y(start, end, x)), color);
@@ -118,31 +121,95 @@ fn draw_rect(img: Image, top_left: Coordinate, top_right: Coordinate) -> Image {
  */
 fn draw_ellipse(img: Image, centre: Coordinate, r_ver: i32, r_hor: i32, filled: bool) -> Image {
     img
-}  
+}
+
+
+fn prompt(prompt_str: String) -> String {
+    if prompt_str != "" {
+        println!("{prompt_str}");
+    }
+    print!(">>> ");
+    let mut response = String::new();
+    io::stdin().read_line(&mut response).unwrap();
+    response.trim().to_string()
+}
+
+
+fn prom_num() -> u32 {
+    prompt(String::new()).parse().unwrap()
+}
+
+
+fn prompt_coord() -> Coordinate {
+    println!("X");
+    let x = prom_num();
+    println!("Y");
+    let y = prom_num();
+    (x, y)
+}
+
+
+fn prompt_main() -> Image {
+    println!("Welcome to CS Paint");
+    println!("Main menu");
+    println!("===================");
+    println!("o => open file");
+    println!("n => new file");
+    println!("===================");
+    print!(">>> ");
+
+    let choice = prompt(String::new());
+    match choice.as_str() {
+        "o" => {
+            let filename = prompt("Enter filename".to_string());
+            return load(&filename);
+        }
+        "n" => {
+            let dims = prompt_coord();
+            return create(Dimensions { width: dims.0, height: dims.1 });
+        }
+        &_ => {
+            println!("Invalid option :(");
+            panic!();
+        }
+    }
+}
+
+
+fn prompt_edit(mut img: Image) -> Image {
+    println!("p => set pixel");
+    println!("l => draw line");
+    println!("s => save");
+
+    let choice = prompt(String::new());
+    match choice.as_str() {
+        "p" => {
+            let coord = prompt_coord();
+            return draw_pixel(img, coord, Pixel::new(255, 255, 255));
+        }
+        "l" => {
+            println!("Start point");
+            let start = prompt_coord();
+            println!("End point");
+            let end = prompt_coord();
+            return draw_line(img, start, end, Pixel::new(255, 255, 255));
+        }
+        "s" => {
+            let filename = prompt("Filename".to_string());
+            save(img, &filename.to_string());
+            exit(0);
+        }
+        &_ => {
+            println!("Invalid option :(");
+            panic!();
+        }
+    }
+}
 
 
 fn main() {
-    let path = std::env::args().nth(1).expect("You must provide a path.");
-
-    // TODO: make configurable
-    let image_dimensions = Dimensions {
-        width: 256,
-        height: 256,
-    };
-
-    let image = create(image_dimensions);
-
-    print!("Which operation? ");
-    // We use "flush" so that we see the question before the answer.
-    // We can only use `flush` when we use `Write` too -- don't worry why yet!
-    stdout().flush().unwrap();
-    let mut op = String::new();
-    std::io::stdin().read_line(&mut op).unwrap();
-
-    // match op.as_str() {
-    //     "pixel\n" => draw_pixel(path.as_str()),
-    //     _ =>  {
-    //         eprintln!("The operation {op} was not recognised!");
-    //     },
-    // }
+    let mut img = prompt_main();
+    loop {
+        img = prompt_edit(img);
+    }
 }
